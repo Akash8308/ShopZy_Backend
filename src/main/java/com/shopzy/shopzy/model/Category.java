bb6e6a0bb5e980b156ee8cabdd143679cd7ebfef
Akash <86683095+Akash8308@users.noreply.github.com>
2026-04-03 03:06:48 +0530

    Added Models

---
+++ src/main/java/com/shopzy/shopzy/model/Category.java
@@ -0,0 +1,28 @@
+package com.shopzy.shopzy.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
---
+++ src/main/java/com/shopzy/shopzy/valueobject/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.shopzy.shopzy.valueobject;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}

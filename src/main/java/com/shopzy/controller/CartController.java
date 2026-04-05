ac4753866a75c9e78ad13bb0dcbd2c0c3abbf8e2
Akash <86683095+Akash8308@users.noreply.github.com>
2026-04-05 23:50:44 +0530

    controller fix

--- src/main/java/com/shopzy/controller/CartController.java
+++ src/main/java/com/shopzy/controller/CartController.java
@@ -2,20 +2,20 @@ package com.shopzy.controller;
 
 import com.shopzy.model.CartItem;
 import com.shopzy.service.CartService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api/cart-items")
 public class CartController {
 
-    private CartService cartService;
+    private final CartService cartService;
 
     public CartController(CartService cartService){
         this.cartService = cartService;
     }
 
-    public CartItem addItemToCart(CartItem cartItem){
+    @PostMapping
+    public CartItem addItemToCart(@RequestBody CartItem cartItem){
         return this.cartService.addItemToCart(cartItem);
     }
-
 }

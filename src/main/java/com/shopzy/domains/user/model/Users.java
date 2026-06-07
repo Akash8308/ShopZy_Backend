84e4eb95e67da4296bf0b165cf578d9c0572bec6
Akash <86683095+Akash8308@users.noreply.github.com>
2026-06-07 19:19:56 +0530

    Rename user to users

--- src/main/java/com/shopzy/domains/user/model/Users.java
+++ src/main/java/com/shopzy/domains/user/model/Users.java
@@ -12,8 +12,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @Entity
-@Table(name = "users")
-public class User {
+public class Users {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

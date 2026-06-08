59395a0523598fef52323ebf0ea0eb42b2375c2d
Akash <86683095+Akash8308@users.noreply.github.com>
2026-06-08 21:37:05 +0530

    Update frontend url

--- src/main/java/com/shopzy/shared/securityConfig/OAuth2SuccessHandler.java
+++ src/main/java/com/shopzy/shared/securityConfig/OAuth2SuccessHandler.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -22,6 +23,9 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
     @Autowired
     private JwtService jwtService;
+
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
 
     @Override
     public void onAuthenticationSuccess (
@@ -46,12 +50,6 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         String token = jwtService.generateToken(user);
 
-//        String url = ServletUriComponentsBuilder
-//                .fromCurrentContextPath()
-//                .path("/home")
-//                .toUriString();
-//
-//        response.sendRedirect(url);
-        response.sendRedirect("https://shopzy-rouge.vercel.app/");
+        response.sendRedirect(frontendUrl);
     }
 }
